73363a623530a26ebdfb3e93e8dbf7b59b6f8bdb
Ethan Miller <ethan.w.miller@gmail.com>
2015-11-29 18:42:36 -0500

    lotsa stuff

--- src/main/java/GraphColoring.java
+++ src/main/java/GraphColoring.java
@@ -11,33 +11,56 @@ public class GraphColoring {
 
   public static void main(String[] args) {
     List<List<Integer>> graph = new ArrayList<List<Integer>>();
-
-    if(args[0].equals("1")){
-      System.out.println("Scanning file: largegraph1");
-      graph = scanLG1();
-    }
-    else if(args[0].equals("2")){
-      System.out.println("Scanning file: largegraph2");
-      graph = scanLG2();
-    }
-    else if(args[0].equals("3")){
-      System.out.println("Scanning file: smallgraph");
-      graph = scanSG();
-    }
-    else {
-      System.exit(0);
-    }
+    graph = scanGraph(args[0]);
     GraphColoring gc = new GraphColoring();
     System.out.println(gc.isTwoColorable(graph));
   }
 
-  private static List<List<Integer>> scanLG1(){
-    return new ArrayList<List<Integer>>();
-  }
-  private static List<List<Integer>> scanLG2(){
-    return new ArrayList<List<Integer>>();
-  }
-  private static List<List<Integer>> scanSG(){
-    return new ArrayList<List<Integer>>();
+  private static List<List<Integer>> scanGraph(String arg){
+    // method for scanning graph file in and converting it to an adjacency list
+
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    List<String> scanned = new ArrayList<String>();
+    String filePath = "";
+
+    if(arg.equals("1")){
+      System.out.println("Scanning file: largegraph1");
+      filePath = String.format("./src/main/resources/largegraph1");
+    }
+    else if(arg.equals("2")){
+      System.out.println("Scanning file: largegraph2");
+      filePath = String.format("./src/main/resources/largegraph2");
+    }
+    else if(arg.equals("3")){
+      System.out.println("Scanning file: smallgraph");
+      filePath = String.format("./src/main/resources/smallgraph");
+    }
+    else {
+      System.exit(0);
+    }
+    try { // file input
+      Scanner fileScanner = new Scanner(new File(filePath));
+      while(fileScanner.hasNextLine()){
+        String str = fileScanner.nextLine();
+        scanned.add(str);
+      }
+    } catch(FileNotFoundException e) {
+      e.printStackTrace(System.out);
+    }
+
+    //initialize adjacency list with correct number of vertices
+    //the 0 index will be empty for ease of reference later
+    int v = Integer.parseInt(scanned.get(0));
+    System.out.println("Total vertices: " + v);
+    for(int i = 0; i <= v; i++){
+      res.add(new ArrayList<Integer>());
+    }
+
+    for(int i = 1; i < scanned.size(); i++){ // for each line of the file
+      String[] line = scanned.get(i).split(" ");
+      int currentVertex = Integer.parseInt(line[0]);
+      res.get(currentVertex).add(Integer.parseInt(line[1]));
+    }
+    return res;
   }
 }
